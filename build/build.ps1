69d5712e79dabe0514f70ecad653f4d2ab8f1d6f
unknown <rea@LT-REA.dk.sitecore.net>
2015-11-04 10:44:30 +1100

    Tidied manifest properties

--- build/build.ps1
+++ build/build.ps1
@@ -44,21 +44,27 @@ Import-Module sqlps -DisableNameChecking
 Pop-Location
 
 ############################################
+# Setup Location variables
+############################################
+$websiteFolder = $Manifest.builds.$buildType.websiteTarget+'\Website'
+$dataFolder = $Manifest.builds.$buildType.websiteTarget+'\Data'
+
+############################################
 # Perform Setup tasks
 ############################################
 EnsureDirExists -dir $Manifest.builds.$buildType.tempDir
-EnsureDirExists -dir $Manifest.builds.$buildType.iisPath
+EnsureDirExists -dir $websiteFolder
 RemoveSite -iisSiteName $Manifest.builds.$buildType.iisSiteName
 DropAllDatabases -dbNamePrefix $Manifest.builds.$buildType.dbNamePrefix -sqlServer $Manifest.builds.$buildType.sqlServer
-CleanExistingSiteRoot -publishTarget $Manifest.builds.$buildType.unzipTarget
-CopyCleanSitecoreInstance -cmsRepository $Manifest.builds.$buildType.cmsRepository -manifest $Manifest -publishTarget $Manifest.builds.$buildType.unzipTarget
+CleanExistingSiteRoot -publishTarget $Manifest.builds.$buildType.websiteTarget
+CopyCleanSitecoreInstance -cmsRepository $Manifest.builds.$buildType.cmsRepository -manifest $Manifest -publishTarget $Manifest.builds.$buildType.websiteTarget
 RestoreNugetPackages -solution $solution
 RestoreNodeModules
 CopySitecoreAssemblies
-BuildSolutionWithPublish -solution $solution -publishTarget $Manifest.builds.$buildType.publishTarget -buildConfiguration $Manifest.builds.$buildType.buildConfiguration
-CopyFiles -sourceFiles $environConfigs -publishTarget $Manifest.builds.$buildType.publishTarget
-CopyFiles -sourceFiles $Manifest.builds.$buildType.licenseFile -publishTarget $Manifest.builds.$buildType.licenseTarget
-AddSite -iisSiteName $Manifest.builds.$buildType.iisSiteName -siteRoot $Manifest.builds.$buildType.iisPath -hostnames $Manifest.builds.$buildType.hostNames
+BuildSolutionWithPublish -solution $solution -publishTarget $websiteFolder -buildConfiguration $Manifest.builds.$buildType.buildConfiguration
+CopyFiles -sourceFiles $environConfigs -publishTarget $websiteFolder
+CopyFiles -sourceFiles $Manifest.builds.$buildType.licenseFile -publishTarget $dataFolder
+AddSite -iisSiteName $Manifest.builds.$buildType.iisSiteName -siteRoot $websiteFolder -hostnames $Manifest.builds.$buildType.hostNames
 CopyDatabaseFiles -cmsRepository $Manifest.builds.$buildType.cmsRepository -manifest $Manifest -dbDataLocation $Manifest.builds.$buildType.dbDataLocation -tempDir $Manifest.builds.$buildType.tempDir -dbNamePrefix $Manifest.builds.$buildType.dbNamePrefix
 CreateDbLogin -username $Manifest.builds.$buildType.dbUsername -password $Manifest.builds.$buildType.dbPassword -sqlServer $Manifest.builds.$buildType.sqlServer
 AttachAllDatabases -dbNamePrefix $Manifest.builds.$buildType.dbNamePrefix -dbDataLocation $Manifest.builds.$buildType.dbDataLocation -sqlServer $Manifest.builds.$buildType.sqlServer
